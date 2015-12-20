921cc958542aeaf5d2695fdb3bbf5c2748c57d56
Paulker <Paulker@Paulker-PC>
2015-12-20 09:44:54 -0500

    Version 0.0.1 Arbol navideño con tres cirucitos de luces

---
+++ src/co/edu/uvp/pri/animation/Main.java
@@ -0,0 +1,28 @@
+package co.edu.uvp.pri.animation;
+
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author zeus
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    new edu.uvpalmira.pri.eval.pt02.Application2().setVisible(true);
+                } catch (IOException ex) {
+                    Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        });
+    }
+
+}
